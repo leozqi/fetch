eeff67b502e299b9a644f85d84c35b190cd8bc56
Leo Qi <leo@leozqi.com>
2024-04-07 14:38:09 -0400

    Add API skeleton using urfave/cli

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+Please see:
+
+- [How to Write Go Code](ttps://go.dev/doc/code)
+- [Effective Go](https://go.dev/doc/effective_go)
+
+```
+gofmt
+```
---
+++ cmd/fetch/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+//	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "fetch",
+		Usage: "The universal package manager",
+		Commands: []*cli.Command{
+			{
+				Name:  "refresh",
+				Usage: "Refresh list of packages",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Refreshed")
+                    return nil
+				},
+			},
+			{
+				Name:  "install",
+				Usage: "Install a package",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Installed ", cCtx.Args().First())
+                    return nil
+				},
+			},
+			{
+				Name:  "update",
+				Usage: "Update an preinstalled package",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Updated ", cCtx.Args().First())
+                    return nil
+				},
+			},
+			{
+				Name:  "uninstall",
+				Usage: "Uninstall an installed package",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Removed ", cCtx.Args().First())
+                    return nil
+				},
+			},
+            {
+				Name:  "list",
+				Usage: "Lists installed packages",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Removed ", cCtx.Args().First())
+                    return nil
+				},
+			},
+            {
+                Name: "view",
+                Usage: "View all files associated with package on computer",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("Viewed", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name: "depends",
+                Usage: "view all dependencies of the package"
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("Depends", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name: "from",
+                Usage: "add new target repository for packages",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("Added new remote", cCtx.Args().First())
+                    return nil
+                },
+            },
+            {
+                Name: "sources",
+                Usage: "view all sources for packages",
+                Action: func(cCtx *cli.Context) error {
+                    fmt.Println("Added new source", cCtx.Args().First())
+                    return nil
+                },
+            },
+		},
+	}
+
+	app.Run(os.Args)
+}
